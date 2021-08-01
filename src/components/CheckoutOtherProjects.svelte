0ca6e144cc175d77c2ccc96c6590b41097622832
David Han <davidjhan96@gmail.com>
2021-08-01 12:49:43 -0700

    Commited cover photos

---
+++ src/components/CheckoutOtherProjects.svelte
@@ -0,0 +1,6 @@
+<script>
+    let cl
+    export { cl as class }
+</script>
+<span class='text-center {cl} text-gray-500'>Check out my other projects at
+    <a class='underline font-semibold text-black' href='https://davjhan.com'>davjhan.com</a></span>
